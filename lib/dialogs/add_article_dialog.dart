b87b54d8aab7b398b4ae489dd326f5a3bc78b489
Hyppolite-DEDEKON <90970777+Hyppolite-DEDEKON@users.noreply.github.com>
2024-01-11 14:37:56 +0100

    Ajoute de contrainte dans les champs

--- lib/dialogs/add_article_dialog.dart
+++ lib/dialogs/add_article_dialog.dart
@@ -1,4 +1,3 @@
-// dialogs/add_article_dialog.dart
 import 'package:flutter/material.dart';
 import '../model/article.dart';
 
@@ -15,6 +14,8 @@ class _AddArticleDialogState extends State<AddArticleDialog> {
   late TextEditingController nameController;
   late TextEditingController categoryController;
   late TextEditingController quantityController;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -33,48 +34,82 @@ class _AddArticleDialogState extends State<AddArticleDialog> {
       title: Text(widget.initialArticle != null
           ? 'Modifier Article'
           : 'Ajouter Article'),
-      content: SizedBox(
-        height: 180.0, // Ajustez la hauteur selon vos besoins
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(labelText: 'Nom'),
-            ),
-            TextField(
-              controller: categoryController,
-              decoration: InputDecoration(labelText: 'Catégorie'),
-            ),
-            TextField(
-              controller: quantityController,
-              decoration: InputDecoration(labelText: 'Quantité'),
-              keyboardType: TextInputType.number,
-            ),
-          ],
+      content: Container(
+        width: double.maxFinite,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(labelText: 'Nom'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Champ requis';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: categoryController,
+                      decoration: InputDecoration(labelText: 'Catégorie'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Champ requis';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: quantityController,
+                      decoration: InputDecoration(labelText: 'Quantité'),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Champ requis';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                  height: 16), // Espacement entre le formulaire et les boutons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Annuler
+                    },
+                    child: Text('Annuler'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Valider le formulaire
+                      if (_formKey.currentState?.validate() ?? false) {
+                        Article updatedArticle = Article(
+                          name: nameController.text,
+                          category: categoryController.text,
+                          quantity: int.tryParse(quantityController.text) ?? 0,
+                          isBought: widget.initialArticle?.isBought ?? false,
+                        );
+                        Navigator.of(context).pop(updatedArticle);
+                      }
+                    },
+                    child: Text('Valider'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop(); // Annuler
-          },
-          child: Text('Annuler'),
-        ),
-        TextButton(
-          onPressed: () {
-            // Valider
-            Article updatedArticle = Article(
-              name: nameController.text,
-              category: categoryController.text,
-              quantity: int.tryParse(quantityController.text) ?? 0,
-              isBought: widget.initialArticle?.isBought ?? false,
-            );
-            Navigator.of(context).pop(updatedArticle);
-          },
-          child: Text('Valider'),
-        ),
-      ],
     );
   }
 }
